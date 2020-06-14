dd4d32184ad6422e9fa4f9d381aa3e842b2204f4
Erik G <>
2020-06-14 15:44:05 +0200

    Update clientId

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   auth0: {
     url: 'erigre.eu', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
-    clientId: '9LBg0xe1UVvVcQyz6QXQIN9y2YhHgdkE', // the client id generated for the auth0 app
+    clientId: 'z1ql1gxKPCxwLMIrEftPSRJmr1Vk6AY1', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
